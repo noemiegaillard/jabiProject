00ebc4d7a28c5ea4cf025d10c6d940641495190f
noemiegaillard <noemie.gaillard@etudiant.univ-rennes1.fr>
2023-07-31 15:16:30 +0200

    change title and folder

---
+++ assets/fonts/roboto.js
@@ -0,0 +1,31 @@
+import * as Font from 'expo-font';
+import { useEffect, useState } from 'react';
+import { Roboto_100Thin,Roboto_100Thin_Italic,Roboto_700Bold_Italic,Roboto_300Light,Roboto_500Medium,Roboto_400Regular,Roboto_300Light_Italic} from '@expo-google-fonts/roboto';
+
+export function useCustomFonts() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFonts() {
+      try {
+        await Font.loadAsync({
+          'RobotoItalic_100': Roboto_100Thin_Italic,
+          'Roboto_100': Roboto_100Thin,
+          'RobotoBoldItalic_700': Roboto_700Bold_Italic,
+          'Roboto_300':Roboto_300Light,
+          'RobotoItalic_300':Roboto_300Light_Italic,
+          'RobotoMedium':Roboto_500Medium
+        });
+
+        setFontsLoaded(true);
+      } catch (error) {
+        console.error('Error loading fonts:', error);
+        setFontsLoaded(false); // Set fontsLoaded to false to handle the error state
+      }
+    }
+
+    loadFonts();
+  }, []);
+
+  return fontsLoaded;
+}
